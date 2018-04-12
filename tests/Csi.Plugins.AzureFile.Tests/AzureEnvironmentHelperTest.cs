92a970194f340cde689c54d42a4570bbe0d97a9b
Dong Liu <doliu@microsoft.com>
2018-04-12 13:56:02 +0800

    Add AzureEnvironmentHelper

---
+++ tests/Csi.Plugins.AzureFile.Tests/AzureEnvironmentHelperTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Csi.Plugins.AzureFile.Tests
+{
+    public class AzureEnvironmentHelperTest
+    {
+        [Fact]
+        public void Basic()
+        {
+            const string azureGlobalCloudSuffix = ".core.windows.net";
+            const string azureGermanCloudSuffix = ".core.cloudapi.de";
+
+            Assert.Equal(azureGlobalCloudSuffix, AzureEnvironmentHelper.GetStorageEndpointSuffix(""));
+            Assert.Equal(azureGlobalCloudSuffix, AzureEnvironmentHelper.GetStorageEndpointSuffix("AzureGlobalCloud"));
+            Assert.Equal(azureGermanCloudSuffix, AzureEnvironmentHelper.GetStorageEndpointSuffix("AzureGermanCloud"));
+            Assert.Equal(azureGermanCloudSuffix, AzureEnvironmentHelper.GetStorageEndpointSuffix("azuregermancloud"));
+            Assert.Throws<Exception>(() => AzureEnvironmentHelper.GetStorageEndpointSuffix("a"));
+        }
+    }
+}
