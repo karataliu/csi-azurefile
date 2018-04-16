51b14b6bccf2da9ce554af2c085fd846cb6e5941
Dong Liu <doliu@microsoft.com>
2018-04-16 16:55:12 +0800

    Update debug flag

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -z "$CSI_ENDPOINT" ]; then
-    rm $CSI_ENDPOINT
+    rm -f $CSI_ENDPOINT
     socat -d UNIX-LISTEN:$CSI_ENDPOINT,fork TCP4:127.0.0.1:10000 2> /tmp/so.log &
 fi
 
