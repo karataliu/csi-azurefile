120e937c56448cf86056ab2e3b28f93269006eeb
Dong Liu <doliu@microsoft.com>
2018-04-16 16:55:12 +0800

    Update debug flag

--- src/Csi.Plugins.AzureFile/InternalsVisibleTo.cs
+++ src/Csi.Plugins.AzureFile/InternalsVisibleTo.cs
@@ -1,4 +1,6 @@
-﻿// For unit test
+﻿#if DEBUG
+// For unit test
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Csi.Plugins.AzureFile.Tests")]
 // For Moq
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DynamicProxyGenAssembly2")]
+#endif
